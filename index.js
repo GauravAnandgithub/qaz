71c728825c3dfb0f49ddef46e948df5ce65bc87d
Saurav Anand <sauravanand243@gmail.com>
2023-10-31 01:30:59 +0530

    zzzzz

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("hello ")
+// var Name = "fiusguoieg";
+// var age = 76;
+// console.log(Name);
+// console.log(age);
+
+// console.log(typeof(Name));
+// console.log(10 + "10");
+// console.log(9 - "5");  //<-------------bug------------->
+// console.log("java" + "script");
+// console.log(" "   + " ");
+// console.log( " " +  0);
+// console.log( "rtye5y" - "dfgkcshb");  //<-----------NaN(not a number)------->
+// console.log( false - true);  //<-------(-1)--------->
+
+// var phone_number = 353784935436;
+// var  namee = "ghbdafh ";
+
+// console.log(isNaN(phone_number)); //<--------for valid phone number------------>
+// console.log(isNaN(namee));
+
+// console.log("enter your number");
+// function add(a, b) {
+//     var c = a + b;
+//     console.log(c);
+//     return c;
+// }
+
+// add(11, 10)
+// add(12, 11)
+// add(13, 12)
+// add(14, 13)
+// add(15, 14)
+
+//  let my = "i am gaurav anand";
+// console.log(my.length);
+// console.log(my.indexOf("am"));
+// console.log(my.lastIndexOf("gaurav"));
+// console.log(my.search("am"));
+
+let my = [i, am , gaurav , anand];
+my.sort()
+console.log(my);
